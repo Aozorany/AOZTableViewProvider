c2cd611846a6b0f855f973269190a5fc8d0dd866
Aozorany <aoisorani@Aozorany-Mac-Mini.local>
2015-11-26 20:05:47 +0800

    first commit

---
+++ AOZTableViewProvider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOZTableViewProvider
+//
+//  Created by Aoisorani on 11/26/15.
+//  Copyright © 2015 Aozorany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
