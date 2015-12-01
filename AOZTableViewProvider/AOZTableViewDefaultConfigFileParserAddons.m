c18ea2a342952debdb05ff839a11fb684b9ed4e4
Aozorany <thirteen2001@163.com>
2015-12-01 23:26:01 +0800

    重构RowCollection的解析方法

--- AOZTableViewProvider/AOZTableViewDefaultConfigFileParserAddons.m
+++ AOZTableViewProvider/AOZTableViewDefaultConfigFileParserAddons.m
@@ -12,6 +12,36 @@
 #import "AOZTableViewCell.h"
 
 
+NSString *getChunkFromMatchesArray(NSString *str, NSArray<NSTextCheckingResult *> *matchesArray, int index);
+NSString *getChunkFromMatchesArray(NSString *str, NSArray<NSTextCheckingResult *> *matchesArray, int index) {
+    if (matchesArray == nil || str == nil) {
+        return nil;
+    }
+    if (index < 0 || index >= matchesArray.count) {
+        return nil;
+    }
+    NSTextCheckingResult *checkingResult = matchesArray[index];
+    NSString *subStr = nil;
+    @try {
+        subStr = [str substringWithRange:checkingResult.range];
+    }
+    @catch (NSException *exception) {
+        subStr = nil;
+    }
+    return subStr;
+}
+
+BOOL stringIsInt(NSString *str);
+BOOL stringIsInt(NSString *str) {
+    if (str.length == 0) {
+        return NO;
+    }
+    NSScanner* scan = [NSScanner scannerWithString:str];
+    int val;
+    return [scan scanInt:&val] && [scan isAtEnd];
+}
+
+
 #pragma mark -
 @implementation AOZTableViewDefaultRowParser
 - (AOZTVPRowCollection *)parseNewConfig:(NSString *)lineStr {
@@ -20,36 +50,31 @@
         return nil;
     }
     
-    //如果不是以row开头则返回空
-    if (![lineStr hasPrefix:@"row "]) {
+    //把表达式分开，并解析每个参数，生成rowCollection
+    NSRegularExpression *regEx = [NSRegularExpression regularExpressionWithPattern:@"-{0,1}\\w+" options:NSRegularExpressionCaseInsensitive error:nil];
+    NSArray<NSTextCheckingResult *> *matchesArray = [regEx matchesInString:lineStr options:0 range:NSMakeRange(0, lineStr.length)];
+    if (matchesArray.count < 1) {
         return nil;
     }
     
-    //把表达式分开，并解析每个参数，生成rowCollection
-    AOZTVPRowCollection *rowCollection = [[AOZTVPRowCollection alloc] init];
-    NSRegularExpression *regEx = [NSRegularExpression regularExpressionWithPattern:@"-{0,1}\\w+" options:NSRegularExpressionCaseInsensitive error:nil];
-    NSArray<NSTextCheckingResult *> *matchesArray = [regEx matchesInString:lineStr options:0 range:NSMakeRange(0, lineStr.length)];
-    if (matchesArray.count <= 1) {
+    //如果第一个部分不是row开头，则返回
+    NSString *prefix = getChunkFromMatchesArray(lineStr, matchesArray, 0);
+    if (![prefix isEqualToString:@"row"]) {
         return nil;
     }
-    BOOL haveAvaliableParams = NO;
+    
+    AOZTVPRowCollection *rowCollection = [[AOZTVPRowCollection alloc] init];
     for (int index = 1; index < matchesArray.count;) {
-        NSTextCheckingResult *checkingResult = matchesArray[index];
-        if (checkingResult.range.location == NSNotFound || checkingResult.range.length == 0) {
-            continue;
-        }
-        NSString *chunk = [lineStr substringWithRange:checkingResult.range];
+        NSString *chunk = getChunkFromMatchesArray(lineStr, matchesArray, index);
         if ([chunk isEqualToString:@"-s"]) {
-            if (index == matchesArray.count - 1) {
+            //-s指示符，下一个参数是数据源
+            if (_dataProvider == nil) {
+                //没有指定数据源的查找范围，返回空
                 return nil;
             }
-            NSTextCheckingResult *nextCheckingResult = matchesArray[index + 1];
-            if (nextCheckingResult.range.location == NSNotFound
-                || nextCheckingResult.range.length == 0) {
-                continue;
-            }
-            NSString *nextChunk = [lineStr substringWithRange:nextCheckingResult.range];
-            if (_dataProvider == nil) {
+            NSString *nextChunk = getChunkFromMatchesArray(lineStr, matchesArray, index + 1);
+            if (nextChunk == nil) {
+                //下一个参数为空，返回空
                 return nil;
             }
             id source = nil;
@@ -57,37 +82,36 @@
                 source = [_dataProvider valueForKey:nextChunk];
             }
             @catch (NSException *exception) {
+                //如果根据参数找到数据源，则返回空
                 return nil;
             }
+            //找到了数据源，则指定之
             rowCollection.dataConfig.source = source;
+            //下一个指示符
             index += 2;
         } else if ([chunk isEqualToString:@"-c"]) {
-            if (index == matchesArray.count - 1) {
+            //-c指示符，下一个参数是单元格类型
+            NSString *nextChunk = getChunkFromMatchesArray(lineStr, matchesArray, index + 1);
+            if (nextChunk == nil) {
                 return nil;
             }
-            NSTextCheckingResult *nextCheckingResult = matchesArray[index + 1];
-            if (nextCheckingResult.range.location == NSNotFound
-                || nextCheckingResult.range.length == 0) {
-                continue;
-            }
-            NSString *nextChunk = [lineStr substringWithRange:nextCheckingResult.range];
             Class cellClass = objc_getClass([nextChunk UTF8String]);
+            if (cellClass == NULL) {
+                return nil;
+            }
             rowCollection.dataConfig.cellClass = cellClass? cellClass: [AOZTableViewCell class];
             index += 2;
         } else if ([chunk isEqualToString:@"-n"]) {
-            if (index == matchesArray.count - 1) {
+            //-n指示符，下一参数是每一行元素个数
+            NSString *nextChunk = getChunkFromMatchesArray(lineStr, matchesArray, index + 1);
+            if (nextChunk == nil || !stringIsInt(nextChunk)) {
                 return nil;
             }
-            NSTextCheckingResult *nextCheckingResult = matchesArray[index + 1];
-            if (nextCheckingResult.range.location == NSNotFound
-                || nextCheckingResult.range.length == 0) {
-                continue;
-            }
-            NSString *nextChunk = [lineStr substringWithRange:nextCheckingResult.range];
             NSInteger elementsPerRow = [nextChunk integerValue];
             rowCollection.dataConfig.elementsPerRow = elementsPerRow > 0? elementsPerRow: 1;
             index += 2;
         } else if ([chunk isEqualToString:@"-all"]) {
+            //-all指示符，所有参数都在同一行中
             rowCollection.dataConfig.elementsPerRow = -1;
             index ++;
         } else {
--- AOZTableViewProviderTests/AOZTableViewDefaultRowParserTest.m
+++ AOZTableViewProviderTests/AOZTableViewDefaultRowParserTest.m
@@ -17,8 +17,11 @@
 
 @implementation AOZTableViewDefaultRowParserTest {
     AOZTableViewDefaultRowParser *_rowParser;
-    NSArray *_datasArray;
-    NSArray *_emptyDatasArray;
+    NSArray *_array;
+    NSArray *_emptyArray;
+    NSArray *_nilArray;
+    NSDictionary *_dictionary;
+    NSDictionary *_emptyDictionary;
 }
 
 #pragma mark setup and teardown
@@ -26,8 +29,12 @@
     [super setUp];
     _rowParser = [[AOZTableViewDefaultRowParser alloc] init];
     _rowParser.dataProvider = self;
-    _datasArray = @[@"1", @"2", @"3"];
-    _emptyDatasArray = @[];
+    
+    _array = @[@"1", @"2", @"3"];
+    _emptyArray = @[];
+    _nilArray = nil;
+    _dictionary = @{@"1": @1, @"2": @2};
+    _emptyDictionary = @{};
 }
 
 - (void)tearDown {
@@ -52,25 +59,55 @@
     rowCollection = [_rowParser parseNewConfig:@"section"];
     NSAssert(rowCollection == nil, @"输入参数为section的时候结果不为空");
     
-    rowCollection = [_rowParser parseNewConfig:@"row"];
-    NSAssert(rowCollection == nil, @"输入参数为row的时候结果不为空");
+    rowCollection = [_rowParser parseNewConfig:@"row -s fakeSource"];
+    NSAssert(rowCollection == nil, @"输入参数为row -s fakeSource的时候结果不为空");
     
-    rowCollection = [_rowParser parseNewConfig:@"row 1 2 3 4 5"];
-    NSAssert(rowCollection == nil, @"输入参数为row 1 2 3 4 5的时候结果不为空");
+    rowCollection = [_rowParser parseNewConfig:@"row -s"];
+    NSAssert(rowCollection == nil, @"输入参数为row -s的时候结果不为空");
+    
+    rowCollection = [_rowParser parseNewConfig:@"row -c"];
+    NSAssert(rowCollection == nil, @"输入参数为row -c的时候结果不为空");
+    
+    rowCollection = [_rowParser parseNewConfig:@"row -s -c"];
+    NSAssert(rowCollection == nil, @"输入参数为row -s -c的时候结果不为空");
+    
+    rowCollection = [_rowParser parseNewConfig:@"row -c -s"];
+    NSAssert(rowCollection == nil, @"输入参数为row -c -s的时候结果不为空");
+    
+    rowCollection = [_rowParser parseNewConfig:@"row -s -all"];
+    NSAssert(rowCollection == nil, @"输入参数为row -s -all的时候结果不为空");
+    
+    rowCollection = [_rowParser parseNewConfig:@"row -s -all -c"];
+    NSAssert(rowCollection == nil, @"输入参数为row -s -all -c的时候结果不为空");
 }
 
 /** 测试合法输入情形 */
 - (void)testRegularLines {
+    //row
     AOZTVPRowCollection *rowCollectionResult = [[AOZTVPRowCollection alloc] init];
-    rowCollectionResult.rowRange = NSMakeRange(0, _datasArray.count);
+    rowCollectionResult.rowRange = NSMakeRange(0, 1);
     rowCollectionResult.dataConfig.cellClass = [AOZTableViewCell class];
-    rowCollectionResult.dataConfig.source = _datasArray;
+    rowCollectionResult.dataConfig.source = nil;
+    rowCollectionResult.dataConfig.elementsPerRow = 1;
+    AOZTVPRowCollection *rowCollection = [_rowParser parseNewConfig:@"row"];
+    NSAssert([rowCollection isEqual:rowCollectionResult], @"row与预期结果不相等");
+    
+    //row 1 2 3 4 5
+    rowCollection = [_rowParser parseNewConfig:@"row 1 2 3 4 5"];
+    NSAssert([rowCollection isEqual:rowCollectionResult], @"row 1 2 3 4 5与预期结果不相等");
+    
+    //row -s
+    /*
+    AOZTVPRowCollection *rowCollectionResult = [[AOZTVPRowCollection alloc] init];
+    rowCollectionResult.rowRange = NSMakeRange(0, _array.count);
+    rowCollectionResult.dataConfig.cellClass = [AOZTableViewCell class];
+    rowCollectionResult.dataConfig.source = _array;
     rowCollectionResult.dataConfig.elementsPerRow = 1;
     AOZTVPRowCollection *rowCollection = [_rowParser parseNewConfig:@"row -s _datasArray"];
     NSAssert([rowCollection isEqual:rowCollectionResult], @"row -s _datasArray与预期结果不相等");
     
     rowCollectionResult.dataConfig.elementsPerRow = 2;
-    rowCollectionResult.rowRange = NSMakeRange(0, ceil(_datasArray.count / 2.0f));
+    rowCollectionResult.rowRange = NSMakeRange(0, ceil(_array.count / 2.0f));
     rowCollection = [_rowParser parseNewConfig:@"row -s _datasArray -n 2"];
     NSAssert([rowCollection isEqual:rowCollectionResult], @"row -s _datasArray -n 2与预期结果不相等");
     
@@ -83,6 +120,7 @@
     rowCollectionResult.rowRange = NSMakeRange(0, 0);
     rowCollection = [_rowParser parseNewConfig:@"row -s _emptyDatasArray -n 2"];
     NSAssert([rowCollection isEqual:rowCollectionResult], @"row -s _emptyDatasArray -n 2与预期结果不相等");
+     */
 }
 
 @end
